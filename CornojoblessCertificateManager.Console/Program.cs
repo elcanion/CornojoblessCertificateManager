efc5e6b9c94e45dab9e92703ddb0a2cdd50d29ff
João Victor <elcaniondev@gmail.com>
2026-01-20 00:44:29 -0300

    begin implementing the certificate service.

---
+++ CornojoblessCertificateManager.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using CornojoblessCertificateManager.Core.Services;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CornojoblessCertificateManager.Console
+{
+    public class Program
+    {
+		public static void Main() {
+			var certs = CertificateService.GetCertificate(StoreLocation.CurrentUser, "");
+		}
+	}
+}
---
+++ CornojoblessCertificateManager.Core/Services/CertificateService.cs
@@ -0,0 +1,24 @@
+﻿using CornojoblessCertificateManager.Core.Model;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CornojoblessCertificateManager.Core.Services
+{
+    public class CertificateService
+    {
+		public static List<CertificateInfo> GetCertificate(StoreLocation location, string allowedIssuer) {
+			using var store = new X509Store(StoreName.My, location);
+			store.Open(OpenFlags.ReadOnly);
+
+			return store.Certificates
+				.Cast<X509Certificate2>()
+				.Where(c => c.NotAfter < DateTime.Now && c.Issuer.Contains(allowedIssuer, StringComparison.OrdinalIgnoreCase))
+				.Select(c => new CertificateInfo {
+					Subject = c.Subject,
+					Issuer = c.Issuer,
+					Thumbprint = c.Thumbprint,
+					Expiration = c.NotAfter,
+					HasPrivateKey = c.HasPrivateKey,
+				}).ToList();
+		}
+    }
+}
