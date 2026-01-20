52482a8181fe775b07dff3f344502a7283d6826e
João Victor <elcaniondev@gmail.com>
2026-01-20 00:00:03 -0300

    begin structuring the application core

--- CornojoblessCertificateManager.Core/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace CornojoblessCertificateManager.Core {
-	public class Class1 {
-
-	}
-}
---
+++ CornojoblessCertificateManager.Core/Model/CertificateInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace CornojoblessCertificateManager.Core.Model
+{
+    public class CertificateInfo
+    {
+		public string Subject { get; init; }
+		public string Issuer { get; init; }
+		public string Thumbprint { get; init; }
+		public bool HasPrivateKey { get; init; }
+		public DateTime Expiration { get; init; }
+
+    }
+}
