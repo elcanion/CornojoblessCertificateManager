4522b4e4623e52a0fe0546ea485317a0adde9ffc
João Victor <elcaniondev@gmail.com>
2026-01-19 23:45:32 -0300

    Add project files.

---
+++ CornojoblessCertificateManager.Core/Model/CertificateInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CornojoblessCertificateManager.Core.Model
+{
+    public class CertificateInfo
+    {
+        public string Subject { get; init; }
+        public string Issuer { get; init; }
+        public string Thumbprint { get; init; }
+        public DateTime Expiration { get; init; }
+    }
+}
