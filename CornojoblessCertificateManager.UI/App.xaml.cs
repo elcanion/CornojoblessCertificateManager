9712404af1f0052e368972e4744c97c7954a12d0
João Victor <elcaniondev@gmail.com>
2026-01-23 21:04:15 -0300

    add di

--- CornojoblessCertificateManager.UI/App.xaml.cs
+++ CornojoblessCertificateManager.UI/App.xaml.cs
@@ -1,6 +1,10 @@
 ﻿using System.Windows;
 using CornojoblessCertificateManager.Core.Model;
+using CornojoblessCertificateManager.Core.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using CornojoblessCertificateManager.UI.ViewModel;
 
 namespace CornojoblessCertificateManager.UI;
 
@@ -9,20 +13,28 @@ namespace CornojoblessCertificateManager.UI;
 /// </summary>
 public partial class App : Application
 {
-	public static IConfiguration Configuration { get; private set; }
-	public static CertificateSettings? Settings { get; private set; }
+	public static IHost AppHost { get; private set; }
+
+	public App() {
+		AppHost = Host.CreateDefaultBuilder()
+			.ConfigureServices((ctx, services) => {
+
+			services.AddSingleton<ICertificateService, CertificateService>();
+			services.AddSingleton(provider =>
+			{
+				var config = ctx.Configuration;
+				return config.GetSection("CertificateSettings").Get<CertificateSettings>()!;
+			});
+			services.AddSingleton<MainWindowViewModel>();
+			services.AddSingleton<MainWindow>();
+		}).Build();
+	}
 
 	protected override void OnStartup(StartupEventArgs e) {
 		base.OnStartup(e);
 
-		Configuration = new ConfigurationBuilder()
-			.SetBasePath(AppContext.BaseDirectory)
-			.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-			.Build();
-
-		Settings = Configuration
-			.GetSection("CertificateSettings")
-			.Get<CertificateSettings>();
+		var mainWindow = AppHost.Services.GetRequiredService<MainWindow>();
+		mainWindow.Show();
 	}
 }
 
